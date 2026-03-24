c565e24c7730185c0ebd27f69df20315ff5901c6
fannybillefalt <fannybillefalt@gmail.com>
2026-03-24 11:23:18 +0100

    comments

--- src/Components/TodoInput.jsx
+++ src/Components/TodoInput.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-function TodoInput({ addTodoList }) {//tar emot propen
-    const [text, setText] = useState("");
+function TodoInput({ addTodoList }) {
+  //tar emot propen från todolist för att adderas här med lagras i todolist där listan finns.
+  // todoinput lagrar textsträngarna som kommer in av användaren
+  const [text, setText] = useState("");
 
-    function handleChange(event){
-        setText(event.target.value);
-    }
+  function handleChange(event) {
+    setText(event.target.value);
+  }
+
+  function handleClick() {
+    addTodoList(text);
+    setText("");
+  }
 
   return (
     <div className="todo-input-container">
-   <input value={text} onChange={handleChange}/>
-   <button onClick={() => {
-    addTodoList(text)
-    setText("")
-}}>Lägg till</button>
+      <input value={text} onChange={handleChange} />
+      <button onClick={handleClick}>Lägg till</button>
     </div>
-  )
+  );
 }
 
-export default TodoInput
+export default TodoInput;
--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -3,10 +3,9 @@ import TodoInput from "./TodoInput";
 import TodoItem from "./TodoItem";
 
 function TodoList() {
-  const [todos, setTodosList] = useState(() => {
-    const sparadeTodos = localStorage.getItem("todos");
-    return sparadeTodos ? JSON.parse(sparadeTodos) : [];
-  }); //läser todos när sidan laddas
+  //todoList lagrar en lista med alla todo
+  const [todos, setTodosList] = useState(JSON.parse(sparadeTodos) || []); //läser todos när sidan laddas, om det finns, omvandla texten tillbaka till en array med JSON.parse,
+  // om det inte finns, börja med en tom lista.
 
   useEffect(() => {
     //sparar todos varje gång listan ändras
@@ -18,10 +17,12 @@ function TodoList() {
   }
 
   function deleteTodo(id) {
+    //behåll alla todos när id inte matchar, den todo som matchar faller bort automatiskt.
     setTodosList(todos.filter((todos) => todos.id !== id));
   }
 
   function toggleTodo(id) {
+    //som en PÅ/AV knapp, är todo klar eller ej.
     setTodosList(
       todos.map((todo) =>
         todo.id === id ? { ...todo, completed: !todo.completed } : todo,
@@ -31,7 +32,7 @@ function TodoList() {
 
   return (
     //skickar ner propen till barnet
-    <div classname="todo-wrapper">
+    <div className="todo-wrapper">
       <h1>Todo lista</h1>
       <TodoInput addTodoList={addTodoList} />
       <div className="todo-list">
